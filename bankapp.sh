dca9860f3e0cb3c195b86aff0557b9f46d46be31
Adeyomola <kayzeel@yahoo.com>
2022-09-19 09:34:28 +0100

    Added email and BVN pattern verification, corrected path of mail_contents file

--- bankapp.sh
+++ bankapp.sh
@@ -8,7 +8,6 @@ homepage() {
 homepage
 
 show_details() {
-
 	echo -e "Your details are \n First name: $first_name \n Last name: $last_name \n Account number: 0143123456"
 }
 
@@ -17,7 +16,7 @@ send_mail() {
 	echo "Subject: Your Bank details and new account number" > mail_contents.txt
 	result="$(show_details)"
 	echo $result >> mail_contents.txt 
-	ssmtp $1 < /home/vagrant/Document/bash_scripting_files/mail_contents.txt
+	ssmtp $1 < mail_contents.txt
 }
 
 openaccount() {
@@ -25,16 +24,41 @@ openaccount() {
 	read -p "First Name: " first_name
 	read -p "Last name: " last_name
 	read -p "BVN: " bvn
-
-	while [[ $bvn -lt 11 ]]
+	
+	bvnregex=[[:digit:]]{11}
+	#verify BVN contains only digits and is 11 digits long
+	until [[ $bvn =~ $bvnregex ]] && [[ ${#bvn} == 11 ]]
 	do
-		read -p "BVN: " bvn
+	
+	if [[ $bvn =~ [^[:digit:]] ]]
+	then
+	echo "BVN should contain only numbers"
+	read -p "Please enter your correct BVN: " bvn
+	
+	elif [[ ${#bvn} -lt 11 ]]
+	then
+	echo "BVN less than 11 digits"
+	read -p "Please enter your correct BVN: " bvn
+	
+	elif [[ ${#bvn} -gt 11 ]]
+	then
+	echo "BVN greater than 11 digits"
+	read -p "Please enter your correct BVN: " bvn
+	fi
 	done
+	###
 
 	read -p "Email: " email
+	#verify email pattern with regex
+	regex=[[:alnum:]]+@[[:alnum:]]+\.[[:alpha:]]{2,4}
+	until [[ $email =~ $regex ]]
+	do
+	read -p "Enter a valid email address: " email
+	done
+	###
 	send_mail $email
 
-	 echo -e "\nThank you. A copy of your account details has been sent to your email address. \nWould you like to see your details? \nYes: \nNo:" 
+	 echo -e "\nThank you. A copy of your account details has been sent to your email address. \nWould you like to see your details? \nyes: \nno:" 
 	 read -p "response: " details
 
 	 if [[ $details == "yes" ]]; then
